454cc7db068ffbcd63f81d44bd63959700e566f6
victor <148868957+victozis@users.noreply.github.com>
2024-10-03 16:14:48 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,22 @@
+criaCartao (
+    'Athletico',
+    'Quem é o maior rival do Athletico?',
+    'Seu principal adversário local é o Coritiba Foot Ball Club.'
+)
+criaCartao (
+    'Athletico',
+   'O que significa a caveira na torcida do Atlético paranaense ?',
+    'Eu Sou Caveira é mais do que uma música; é um grito de união e pertencimento que ressoa nas arquibancadas, fortalecendo o vínculo entre o clube e seus seguidores.'
+)
+
+criaCartao (
+    'Athletico',
+   'Quantos títulos oficiais o Athletico tem ?',
+    '50 sendo eles: 1 intercontinental, 2 continentais, 4 nacionais, 38 estaduais e 5 municipais.'
+)
+criaCartao (
+    'Athletico',
+   'Quanto custa o Atlético Paranaense ?',
+    'Em ranking de valor de marcas de clubes de futebol mais valiosas divulgado em 2022 por consultoria especializada (Sports Value), o Athletico é a quinta marca mais valiosa (2,094 Bilhões) do Brasil e a primeira da Região Sul).'
+)
+
